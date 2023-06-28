fe447789f5bcacb9accb047975a207af28b7e8fc
WSL-ZYL <zylzyl2333@gmail.com>
2023-06-28 17:43:33 +0800

    Primes.c but end at 13 and cannot stop.

--- user/primes.c
+++ user/primes.c
@@ -7,37 +7,10 @@ void recur(int p);
 int
 main()
 {
-    // int p[2];
-    // pipe(p);
-    // if(fork()==0){
-    //     recur(p[0]);
-    // }else{
-    //     int i;
-    //     for(i=2; i<=35; i++){
-    //         write(p[1], &i, sizeof(i));
-    //     }
-    //     close(p[1]);
-    //     wait((int *)0);
-    // }
-    // exit(0);
-
     int p[2];
     pipe(p);
     if(fork()==0){
-        int n, i, pn[2];
-        if(!read(p[0], &n, sizeof(n))){
-            exit(1);
-        }
-        fprintf(1, "prime %d\n", n);
-        pipe(pn);
-        while(read(p[0], &i, sizeof(i))){
-            if (i % n != 0){
-                write(pn[1], &i, sizeof(i));
-            }
-        }
-        close(p[0]);
-        close(pn[1]);
-        exit(0);
+        recur(p[0]);
     }else{
         int i;
         for(i=2; i<=35; i++){
@@ -46,7 +19,8 @@ main()
         close(p[1]);
         wait((int *)0);
     }
-    exit(0);    
+    exit(0);
+
 }
 
 void
@@ -54,22 +28,27 @@ recur(int p)
 {
     int pn[2];
     int n, i;
-    pipe(pn);
     if(!read(p, &n, sizeof(n))){
-        exit(1);
+        close(p);
+        exit(0);
     }
-    if(fork()==0){
-        recur(pn[0]);
-    }else{
-        fprintf(1, "prime %d\n", n);
-        while(read(p, &i, sizeof(i))){
+    pipe(pn);
+    fprintf(1, "prime %d\n", n);
+    while(read(p, &i, sizeof(i))){
             if (i % n != 0){
                 write(pn[1], &i, sizeof(i));
             }
         }
         close(p);
         close(pn[1]);
+    if(fork()==0){
+        
+        recur(pn[0]);
+    }else{
+        
+        
         wait((int *)0);
+        exit(0);
     }
     
 }
